6ea53d24f550f520ea18553605c86542d6cf2eae
pathik424 <p4pathik424@gmail.com>
2025-03-31 13:06:11 +0530

    Add new Folder

---
+++ Github Command/functions.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+Github Code : 
+
+git init
+git remote add origin https://github.com/pathik424/grafic-trafic.git
+git remote -v
+git add .
+git commit -m "Initial commit or update"
+git branch -M main  # Rename branch to main (if needed)
+git push -u origin main
+
+git add .
+git commit -m "Your update message"
+git push origin main
+
+?>
